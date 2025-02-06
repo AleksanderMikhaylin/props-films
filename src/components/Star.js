a2e7a05bfb443679f09480ad30926c5ded037f4b
AleksanderMikhaylin <142292071+AleksanderMikhaylin@users.noreply.github.com>
2025-02-07 07:45:49 +1100

    Update Star.js

--- src/components/Star.js
+++ src/components/Star.js
@@ -1,12 +1,19 @@
-function Star(props){
+import React from 'react';
+
+interface StarProps {
+  size?: number;
+  color?: string;
+}
+
+const Star: React.FC<StarProps> = ({ size = 28, color = '#D3BCA2' }) => {
   return (
     <li className="Star">
-    <svg fill="#D3BCA2" height="28" viewBox="0 0 18 18" width="28" xmlns="http://www.w3.org/2000/svg">
-      <path d="M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z"/>
-      <path d="M0 0h18v18H0z" fill="none"/>
-    </svg>
-  </li>
+      <svg fill={color} height={size} viewBox="0 0 18 18" width={size} xmlns="http://www.w3.org/2000/svg">
+        <path d="M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z"/>
+        <path d="M0 0h18v18H0z" fill="none"/>
+      </svg>
+    </li>
   );
-}
+};
 
 export default Star;
